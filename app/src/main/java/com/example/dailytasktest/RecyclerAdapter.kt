f8803b32a662f9f4a408ad3d2f4227db8aeb86ef
SirArchy <wnbemail123@googlemail.com>
2022-08-18 21:52:07 +0200

    started implementation of recycler View

---
+++ app/src/main/java/com/example/dailytasktest/RecyclerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.dailytasktest
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dailytasktest.ui.home.CustomAdapter
+
+class RecyclerAdapter(private val dataSet: Array<String>) :
+    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomAdapter.ViewHolder {
+        // Create a new view, which defines the UI of the list item
+
+    }
+
+    override fun onBindViewHolder(holder: CustomAdapter.ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+}
